885343a04c2691d560ba7de7e29e32fd6fb37e47
AlexG <gavrilenkoalex154@gmail.com>
2023-05-14 15:46:51 +0700

    add ex

---
+++ scripts/process_data.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+import re
+
+
+def get_desc(df):
+    phys_embed = list(df.repr)
+    ref_pattern = re.compile(r'\[ [\S\n\t\v ]*?\]')
+    all_features = []
+    for item in phys_embed:
+        ref_content = ref_pattern.findall(item)
+        descr_seq = []
+        for feature in ref_content:
+            arr = np.array([float(ch) for ch in re.sub(r'\[|\]|\n', "", 
+                                                       feature).split()])
+            descr_seq.append(arr.reshape(1, -1))
+        descr_seq = np.concatenate(descr_seq, axis=0)
+        all_features.append(descr_seq)
+    for item in all_features:
+        assert item.shape == (24, 40)
+    return all_features
+
+
